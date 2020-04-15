3fd32fd5357db9633edd002d332cf28aee1a77b4
imnetcat <guskov.danil@gmail.com>
2020-04-15 03:16:40 +0300

    добавил простой шаблон для юнит-тестов

--- test.cpp
+++ test.cpp
@@ -1,22 +1,16 @@
-#include "nums.h"
-#include "rational.h"
+п»ї#include "rational.h"
 
 int main()
 {
 	// TODO: unit-tests
 
 	{
-		// Тестирование работы рациональных дробей
+		// 
 		try
 		{
-			// тестирование конструктора
-			Rational r1;
-			if (r1.Denominator() != 1 || r1.Numerator() != 0)
-			{
-
-			}
+			
 		}
-		catch (exeption& ex)
+		catch (exception& ex)
 		{
 
 		}
